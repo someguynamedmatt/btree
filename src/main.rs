171ef3cb237829f9b8410641f0e4d99ba15732bf
Matt <dev@mttyng.com>
2019-03-27 21:29:24 -0700

    Working order, splitting into a lib

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,46 @@ use std::vec::Vec;
 #[derive(Debug)]
 struct BTree<K> {
     root_nodes: Vec<Box<Node<K>>>,
-    values: Vec<K>
+    order: u32
 }
 
 impl<K> BTree<K> {
-    fn new(root_nodes: Vec<Box<Node<K>>>, values: Vec<K>) -> BTree<K> {
-        BTree { root_nodes: root_nodes, values: values }
+    fn new_empty(order: u32) -> BTree<K> {
+        BTree { root_nodes: vec![], order: order }
+    }
+
+    fn new_init(values: Vec<K>, order: u32) -> BTree<K> {
+        let new_node: Node<K> = Node::new(vec![], order, values);
+        BTree { root_nodes: vec![Box::new(new_node)], order: order }
+    }
+}
+
+trait Tree<K> {
+    fn push_empty_node(&mut self);
+    fn push_node_with_vals(&mut self, values: Vec<K>);
+}
+
+impl<K> Tree<K> for BTree<K> {
+    fn push_empty_node(&mut self) {
+        let empty_node: Node<K> = Node::new(vec![], self.order, vec![]);
+        self.root_nodes.push(Box::new(empty_node));
+    }
+    fn push_node_with_vals(&mut self, values: Vec<K>) {
+        let new_node: Node<K> = Node::new(vec![], self.order, values);
+        self.root_nodes.push(Box::new(new_node));
     }
 }
 
 #[derive(Debug)]
 struct Node<K> {
     child_nodes: Vec<Box<Node<K>>>,
+    max_children: u32,
     values: Vec<K>
 }
 
 impl<K> Node<K> {
-    fn new(child_nodes: Vec<Box<Node<K>>>, values: Vec<K>) -> Node<K> {
-        Node { child_nodes: child_nodes, values: values }
+    fn new(child_nodes: Vec<Box<Node<K>>>, max_children: u32, values: Vec<K>) -> Node<K> {
+        Node { child_nodes: child_nodes, max_children: max_children, values: values }
     }
 }
 
@@ -43,11 +65,10 @@ impl<K> TreeNode<K> for Node<K> where K: Clone + PartialOrd {
 
 fn main() {
     println!("B+Tree Implementation");
-    let mut tree: BTree<u32> = BTree::new(vec![], vec![1]);
-    let n1: Node<u32> = Node::new(vec![], vec![234]);
-    let mut n2: Node<u32> = Node::new(vec![], vec![2329234]);
-    n2.child_nodes.push(Box::new(Node::new(vec![], vec![8])));
-    tree.root_nodes.push(Box::new(n1));
-    tree.root_nodes.push(Box::new(n2));
+    let mut tree: BTree<u32> = BTree::new_empty(3);
+    let tree2: BTree<u32> = BTree::new_init(vec![1,2,3], 3);
+    tree.push_empty_node();
+
     println!("{:#?}", tree);
+    println!("{:#?}", tree2);
 }
